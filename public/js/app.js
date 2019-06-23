c0bde5716e337f55db9fec5542a873b87ee95e72
janithapm <janithaprabath0@gmail.com>
2019-06-23 21:54:39 +0530

    created fetch request to get weather data

--- public/js/app.js
+++ public/js/app.js
@@ -7,15 +7,32 @@ fetch('http://puzzle.mead.io/puzzle').then((response)=>{
     })
 })
 
-fetch('http://localhost:3000/weather?address=panadura').then((response)=>{
+const weatherForm = document.querySelector('form')
+const weatherSearch = document.querySelector('input')
+const messageOne = document.getElementById('message-1')
+const messageTwo = document.getElementById('message-2')
+
+weatherForm.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    weatherSearchQuery = weatherSearch.value
+
+    messageOne.textContent = " loading..."
+    messageTwo.textContent = ""
+
+    if(weatherSearchQuery!= null){
+        fetch('http://localhost:3000/weather?address='+weatherSearchQuery).then((response)=>{
     response.json().then((data={})=>{
         if (data.error){
-            console.log(data.error)
+            messageOne.textContent = weatherSearchQuery
+            messageTwo.textContent = data.error 
         }
         else{
-            console.log(data.response)
-            
+            messageOne.textContent = data.location
+            messageTwo.textContent = data.response  
+                  
         }
     })
 })
-
+    }
+    
+})
