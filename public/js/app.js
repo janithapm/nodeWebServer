c8d6e53ed717ea147ac2a22f8bd767994939f415
janithapm <janithaprabath0@gmail.com>
2019-10-13 11:03:44 +0530

    minor changes

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,8 @@
 
 console.log('Client side javascript file is loaded!')
 
-fetch('http://puzzle.mead.io/puzzle').then((response)=>{
-    response.json().then((data)=>{
+fetch('http://puzzle.mead.io/puzzle').then((response) => {
+    response.json().then((data) => {
         console.log(data)
     })
 })
@@ -12,27 +12,27 @@ const weatherSearch = document.querySelector('input')
 const messageOne = document.getElementById('message-1')
 const messageTwo = document.getElementById('message-2')
 
-weatherForm.addEventListener('submit',(e)=>{
+weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     weatherSearchQuery = weatherSearch.value
 
     messageOne.textContent = " loading..."
     messageTwo.textContent = ""
 
-    if(weatherSearchQuery!= null){
-        fetch('http://localhost:3000/weather?address='+weatherSearchQuery).then((response)=>{
-    response.json().then((data={})=>{
-        if (data.error){
-            messageOne.textContent = weatherSearchQuery
-            messageTwo.textContent = data.error 
-        }
-        else{
-            messageOne.textContent = data.location
-            messageTwo.textContent = data.response  
-                  
-        }
-    })
-})
+    if (weatherSearchQuery != null) {
+        fetch('http://localhost:3000/weather?address=' + weatherSearchQuery).then((response) => {
+            response.json().then((data = {}) => {
+                if (data.error) {
+                    messageOne.textContent = weatherSearchQuery
+                    messageTwo.textContent = data.error
+                }
+                else {
+                    messageOne.textContent = data.location
+                    messageTwo.textContent = data.response
+
+                }
+            })
+        })
     }
-    
+
 })
